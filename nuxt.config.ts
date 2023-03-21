5287768863554bd743281635ab6f42faa92ff8ef
devdewa <uchiasasuke123.sp@gmail.com>
2023-03-22 01:33:44 +0700

    Changed favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,7 +7,7 @@ export default defineNuxtConfig({
         { name: 'viewport', content: 'width=device-width, initial-scale=1' },
         { hid: 'description', name: 'description', content: 'Welcome to Wangyland Website' }
       ]
-    }
+    },
   },
   
   modules: [
--- pages/store.vue
+++
@@ -1,3 +0,0 @@
-<template>
-  <div class="flex justify-center items-center min-h-screen text-7xl font-semibold">Store Page</div>
-</template>
