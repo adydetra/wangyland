f7a8c1ec6904535e57d8257e81c45d5740873f6a
devdewa <uchiasasuke123.sp@gmail.com>
2023-03-22 16:09:10 +0700

    Update sidebar design

--- components/Sidebar.vue
+++ components/Sidebar.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="flex flex-col mb-12">
-    <NuxtLink to="/guides/kit" class="hover:bg-slate-700 text-2xl outfit-600 py-12 pl-6 bg-slate-600 border-l-4"># KIT</NuxtLink>
-    <NuxtLink to="/guides/warp" class="hover:bg-slate-700 text-2xl outfit-600 py-12 pl-6 bg-slate-600 border-l-4"># Warp</NuxtLink>
-    <NuxtLink to="/guides/claim" class="hover:bg-slate-700 text-2xl outfit-600 py-12 pl-6 bg-slate-600 border-l-4"># Land Claim</NuxtLink>
-    <NuxtLink to="/guides/sethome" class="hover:bg-slate-700 text-2xl outfit-600 py-12 pl-6 bg-slate-600 border-l-4"># Sethome</NuxtLink>
-    <NuxtLink to="/guides/teleport" class="hover:bg-slate-700 text-2xl outfit-600 py-12 pl-6 bg-slate-600 border-l-4"># Teleport</NuxtLink>
-    <NuxtLink to="/guides/Other" class="hover:bg-slate-700 text-2xl outfit-600 py-12 pl-6 bg-slate-600 border-l-4"># Other</NuxtLink>
+    <NuxtLink to="/guides/kit" class="hover:bg-slate-700 text-2xl outfit-600 py-12 pl-6 bg-slate-800 border-indigo-400 border-l-4"># KIT</NuxtLink>
+    <NuxtLink to="/guides/warp" class="hover:bg-slate-700 text-2xl outfit-600 py-12 pl-6 bg-slate-800 border-indigo-400 border-l-4"># Warp</NuxtLink>
+    <NuxtLink to="/guides/claim" class="hover:bg-slate-700 text-2xl outfit-600 py-12 pl-6 bg-slate-800 border-indigo-400 border-l-4"># Land Claim</NuxtLink>
+    <NuxtLink to="/guides/sethome" class="hover:bg-slate-700 text-2xl outfit-600 py-12 pl-6 bg-slate-800 border-indigo-400 border-l-4"># Sethome</NuxtLink>
+    <NuxtLink to="/guides/teleport" class="hover:bg-slate-700 text-2xl outfit-600 py-12 pl-6 bg-slate-800 border-indigo-400 border-l-4"># Teleport</NuxtLink>
+    <NuxtLink to="/guides/Other" class="hover:bg-slate-700 text-2xl outfit-600 py-12 pl-6 bg-slate-800 border-indigo-400 border-l-4"># Other</NuxtLink>
   </div>
 </template>
