1818b62ddd822d778a0f8b71b05847824df29190
devdewa <uchiasasuke123.sp@gmail.com>
2023-03-24 06:06:57 +0700

    Better responsive in guide and unhidden class sidebar

--- pages/guide/warp.vue
+++ pages/guide/warp.vue
@@ -3,7 +3,7 @@
     <div class="lg:col-span-1 col-span-6 pl-3 pr-6 lg:pl-0 lg:pr-0">
       <Sidebar />
     </div>
-    <div class="col-span-6 lg:col-span-3 lg:px-12 px-3">
+    <div class="col-span-6 lg:col-span-3 lg:px-12 px-3 pb-12">
       <h2 id="guide-kit" class="lg:text-6xl text-4xl outfit-800 border-b lg:pb-8 pb-2 lg:mb-4 mb-2 border-gray-700">Warp</h2>
     
         <p class="py-2">Warp is where the admin is set, for example if you need to go to a tutorial then you can type /warp lobby</p>
