c73bb35db970136dd0bd6ed0398fdbf9c880c8d6
devdewa <uchiasasuke123.sp@gmail.com>
2023-03-24 04:37:13 +0700

    Add hover in navigation and footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -2,10 +2,10 @@
   <section class="px-3 lg:px-0 border-y border-slate-900 mt-auto text-gray-400 outfit-300 tracking-wide">
     <div class="container mx-auto py-12 flex justify-between">
       <div>
-        <NuxtLink to="https://dewaprogramming.vercel.app/" target="_blank" class="text-sm"><Icon class="mr-2 text-lg leading-none -mt-1" size="16" name="octicon:octoface" />Powered By Dewa</NuxtLink>
+        <NuxtLink to="https://dewaprogramming.vercel.app/" target="_blank" class="text-sm hover:text-indigo-300"><Icon class="mr-2 text-lg leading-none -mt-1" size="16" name="octicon:octoface" />Powered By Dewa</NuxtLink>
       </div>
       <div>
-        <NuxtLink to="https://github.com/devdewa/wangyland-nuxt" target="_blank"><Icon class="text-lg leading-none -mt-1" size="16" name="octicon:mark-github-16" /></NuxtLink>
+        <NuxtLink to="https://github.com/devdewa/wangyland-nuxt" target="_blank"><Icon class="text-lg leading-none -mt-1 hover:text-indigo-300" size="16" name="octicon:mark-github-16" /></NuxtLink>
       </div>
     </div>
   </section>
