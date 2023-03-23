90fd7973333bdb11b41e119d4f0f6735b40bfedf
devdewa <uchiasasuke123.sp@gmail.com>
2023-03-23 23:43:21 +0700

    Update sidebar, add icon and better design

--- components/Sidebar.vue
+++ components/Sidebar.vue
@@ -1,16 +1,16 @@
 <template>
   <div class="flex flex-col mb-6">
-    <h2 class="outfit-600 text-lg py-3">Getting Started</h2>
-    <NuxtLink to="#" class="hover:bg-slate-700 text-sm outfit-200 py-3 pl-3 bg-slate-800 border-gray-700 border-l">-</NuxtLink>
-    <NuxtLink to="#" class="hover:bg-slate-700 text-sm outfit-200 py-3 pl-3 bg-slate-800 border-gray-700 border-l">-</NuxtLink>
+    <h2 class="outfit-600 text-lg py-3"><Icon class="mr-2 leading-none -mt-1" size="18" name="octicon:play-16" />Getting Started</h2>
+    <NuxtLink to="#" class="hover:bg-slate-700 text-sm outfit-200 py-3 pl-3 ml-2 bg-slate-800 border-gray-700 border-l">-</NuxtLink>
+    <NuxtLink to="#" class="hover:bg-slate-700 text-sm outfit-200 py-3 pl-3 ml-2 bg-slate-800 border-gray-700 border-l">-</NuxtLink>
   </div>
   <div class="flex flex-col mb-6">
-    <h2 class="outfit-600 text-lg py-3">Plugins</h2>
-    <NuxtLink to="/guides/kit" class="hover:bg-slate-700 text-sm outfit-200 py-3 pl-3 bg-slate-800 border-gray-700 border-l guides">KIT</NuxtLink>
-    <NuxtLink to="/guides/warp" class="hover:bg-slate-700 text-sm outfit-200 py-3 pl-3 bg-slate-800 border-gray-700 border-l guides">Warp</NuxtLink>
-    <NuxtLink to="/guides/claim" class="hover:bg-slate-700 text-sm outfit-200 py-3 pl-3 bg-slate-800 border-gray-700 border-l guides">Land Claim</NuxtLink>
-    <NuxtLink to="/guides/sethome" class="hover:bg-slate-700 text-sm outfit-200 py-3 pl-3 bg-slate-800 border-gray-700 border-l guides">Sethome</NuxtLink>
-    <NuxtLink to="/guides/teleport" class="hover:bg-slate-700 text-sm outfit-200 py-3 pl-3 bg-slate-800 border-gray-700 border-l guides">Teleport</NuxtLink>
-    <NuxtLink to="/guides/Other" class="hover:bg-slate-700 text-sm outfit-200 py-3 pl-3 bg-slate-800 border-gray-700 border-l guides">Other</NuxtLink>
+    <h2 class="outfit-600 text-lg py-3"><Icon class="mr-2 leading-none -mt-1" size="18" name="octicon:cpu-16" />Plugins</h2>
+    <NuxtLink to="/guides/kit" class="hover:bg-slate-700 text-sm outfit-200 py-3 pl-3 ml-2 bg-slate-800 border-gray-700 border-l guides">KIT</NuxtLink>
+    <NuxtLink to="/guides/warp" class="hover:bg-slate-700 text-sm outfit-200 py-3 pl-3 ml-2 bg-slate-800 border-gray-700 border-l guides">Warp</NuxtLink>
+    <NuxtLink to="/guides/claim" class="hover:bg-slate-700 text-sm outfit-200 py-3 pl-3 ml-2 bg-slate-800 border-gray-700 border-l guides">Land Claim</NuxtLink>
+    <NuxtLink to="/guides/sethome" class="hover:bg-slate-700 text-sm outfit-200 py-3 pl-3 ml-2 bg-slate-800 border-gray-700 border-l guides">Sethome</NuxtLink>
+    <NuxtLink to="/guides/teleport" class="hover:bg-slate-700 text-sm outfit-200 py-3 pl-3 ml-2 bg-slate-800 border-gray-700 border-l guides">Teleport</NuxtLink>
+    <NuxtLink to="/guides/Other" class="hover:bg-slate-700 text-sm outfit-200 py-3 pl-3 ml-2 bg-slate-800 border-gray-700 border-l guides">Other</NuxtLink>
   </div>
 </template>
