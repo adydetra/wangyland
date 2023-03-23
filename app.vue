10e36828a43fa95cfef8c949447021e8d8f85de1
devdewa <uchiasasuke123.sp@gmail.com>
2023-03-23 23:30:33 +0700

    Update sidebar guide and add active class on page

--- app.vue
+++ app.vue
@@ -12,24 +12,30 @@
 </template>
 
 <style>
+  .router-link-active {
+    @apply text-gray-50 font-bold;
+  }
+  .guides.router-link-active {
+    @apply text-indigo-500 outfit-600;
+  }
   .outfit-200 {
     font-family: Outfit !important;
-    font-weight: 200 !important;
+    font-weight: 200;
   }
   .outfit-300 {
     font-family: Outfit !important;
-    font-weight: 300 !important;
+    font-weight: 300;
   }
   .outfit-400 {
     font-family: Outfit !important;
-    font-weight: 400 !important;
+    font-weight: 400;
   }
   .outfit-500 {
     font-family: Outfit !important;
-    font-weight: 500 !important;
+    font-weight: 500;
   }
   .outfit-600 {
     font-family: Outfit !important;
-    font-weight: 600 !important;
+    font-weight: 600;
   }
 </style>
--- components/Sidebar.vue
+++ components/Sidebar.vue
@@ -1,10 +1,16 @@
 <template>
-  <div class="flex flex-col mb-12">
-    <NuxtLink to="/guides/kit" class="hover:bg-slate-700 text-1xl outfit-600 py-10 pl-6 bg-slate-800 border-indigo-400 border-l-4"># KIT</NuxtLink>
-    <NuxtLink to="/guides/warp" class="hover:bg-slate-700 text-1xl outfit-600 py-10 pl-6 bg-slate-800 border-indigo-400 border-l-4"># Warp</NuxtLink>
-    <NuxtLink to="/guides/claim" class="hover:bg-slate-700 text-1xl outfit-600 py-10 pl-6 bg-slate-800 border-indigo-400 border-l-4"># Land Claim</NuxtLink>
-    <NuxtLink to="/guides/sethome" class="hover:bg-slate-700 text-1xl outfit-600 py-10 pl-6 bg-slate-800 border-indigo-400 border-l-4"># Sethome</NuxtLink>
-    <NuxtLink to="/guides/teleport" class="hover:bg-slate-700 text-1xl outfit-600 py-10 pl-6 bg-slate-800 border-indigo-400 border-l-4"># Teleport</NuxtLink>
-    <NuxtLink to="/guides/Other" class="hover:bg-slate-700 text-1xl outfit-600 py-10 pl-6 bg-slate-800 border-indigo-400 border-l-4"># Other</NuxtLink>
+  <div class="flex flex-col mb-6">
+    <h2 class="outfit-600 text-lg py-3">Getting Started</h2>
+    <NuxtLink to="#" class="hover:bg-slate-700 text-sm outfit-200 py-3 pl-3 bg-slate-800 border-gray-700 border-l">-</NuxtLink>
+    <NuxtLink to="#" class="hover:bg-slate-700 text-sm outfit-200 py-3 pl-3 bg-slate-800 border-gray-700 border-l">-</NuxtLink>
+  </div>
+  <div class="flex flex-col mb-6">
+    <h2 class="outfit-600 text-lg py-3">Plugins</h2>
+    <NuxtLink to="/guides/kit" class="hover:bg-slate-700 text-sm outfit-200 py-3 pl-3 bg-slate-800 border-gray-700 border-l guides">KIT</NuxtLink>
+    <NuxtLink to="/guides/warp" class="hover:bg-slate-700 text-sm outfit-200 py-3 pl-3 bg-slate-800 border-gray-700 border-l guides">Warp</NuxtLink>
+    <NuxtLink to="/guides/claim" class="hover:bg-slate-700 text-sm outfit-200 py-3 pl-3 bg-slate-800 border-gray-700 border-l guides">Land Claim</NuxtLink>
+    <NuxtLink to="/guides/sethome" class="hover:bg-slate-700 text-sm outfit-200 py-3 pl-3 bg-slate-800 border-gray-700 border-l guides">Sethome</NuxtLink>
+    <NuxtLink to="/guides/teleport" class="hover:bg-slate-700 text-sm outfit-200 py-3 pl-3 bg-slate-800 border-gray-700 border-l guides">Teleport</NuxtLink>
+    <NuxtLink to="/guides/Other" class="hover:bg-slate-700 text-sm outfit-200 py-3 pl-3 bg-slate-800 border-gray-700 border-l guides">Other</NuxtLink>
   </div>
 </template>
