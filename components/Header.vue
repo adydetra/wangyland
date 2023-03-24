e1d2579356524aebb5f489cb6e9bd857d8b1e2ce
devdewa <uchiasasuke123.sp@gmail.com>
2023-03-24 07:00:15 +0700

    Update header font

--- components/Header.vue
+++ components/Header.vue
@@ -1,11 +1,11 @@
 <template>
   <!-- Header -->
-  <header class="bg-indigo-600 text-gray-900 py-3 px-4 outfit-400">
+  <header class="bg-indigo-600 text-gray-900 py-3 px-4">
     <div class="container mx-auto flex justify-between items-center">
       <p class="d-flex me-4 mb-0">
-        <strong class="text-indigo-100 text-xs lg:text-lg"><Icon class="mr-3 -mt-1" name="🚀" />Welcome to Wangyland Website</strong>
+        <p class="text-indigo-100 text-xs lg:text-lg outfit-800"><Icon class="mr-3 -mt-1" name="🚀" />Welcome to Wangyland Website</p>
       </p>
-      <p class="text-indigo-100 font-bold tracking-wide hidden lg:block">IP: WANGYLANDID.ATERNOS.ME<Icon class="ml-2 text-lg leading-none -mt-1" size="26" name="uim:pagelines" /></p>
+      <p class="text-indigo-100 outfit-600 tracking-wide hidden lg:block">IP: WANGYLANDID.ATERNOS.ME<Icon class="ml-2 text-lg leading-none -mt-1" size="26" name="uim:pagelines" /></p>
     </div>
   </header>
 </template>
