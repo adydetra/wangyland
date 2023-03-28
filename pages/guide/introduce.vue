2d0de57b43b6ec1bd41b320eaf608d7ce07ae1fd
devdewa <uchiasasuke123.sp@gmail.com>
2023-03-28 21:16:44 +0700

    Update Introduce content

--- pages/guide/introduce.vue
+++ pages/guide/introduce.vue
@@ -9,9 +9,21 @@
         <p class="pt-2 outfit-600 text-xl">Wangyland</p>
   
         <p class="mt-3 leading-8">  
-            The Wangyland server is currently hosted in Indonesia. This You can play
-            Adventure, Arena, Discord, Economy, Land Claim, Ranks, SMP, Survival
-            on the Wangyland server. 
+          Welcome to our minecraft server where you can start your own survival journey with us. In here, you can join our nice SMP Community to play together and thrive. We are happy to have new members join us and be part of our server. 
+        </p>
+
+        <p class="pb-2 mt-8 outfit-600 text-xl">Why join our Server?</p>
+
+        <p class="mt-3 leading-8">
+          Our server is a Survival Multiplayer Server, so you can only play survival Minecraft. But there is still many things you can do that are beyond just a normal survival gameplay. You can make shops and trade your valuable items, make farm to get money, and battle toe to toe with any other player in our arena. We also allow player to sell or buy/rent claim lands or buildings, to make or be part of a town, including our own.
+        </p>
+
+        <p class="mt-3 leading-8">
+          We strive to make the server better and improve time by time with the help of everyone in our community.
+        </p>
+
+        <p class="mt-3 leading-8">
+          Now what are you waiting for?  Join us and make your own adventure by joining our minecraft server and also our discord server. I hope to see you then!
         </p>
 
       </div>
