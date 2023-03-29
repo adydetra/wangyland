810bdbd51198ee691f20d517622872e5b15c0db1
devdewa <uchiasasuke123.sp@gmail.com>
2023-03-29 10:45:54 +0700

    Update hero image background add backdrop blur effect

--- pages/index.vue
+++ pages/index.vue
@@ -11,12 +11,12 @@
             on the Wangyland server.
           </p>
 
-          <p class="lg:hidden text-sm my-3 leading-7 outfit-200">
+          <p class="lg:hidden text-sm my-3 leading-7 outfit-200 ">
             The Wangyland server is currently hosted in Indonesia. This You can play Adventure, Arena, Discord, Economy, Land Claim, Ranks, SMP, Survival<br>
             on the Wangyland server.
           </p>
 
-          <p class="mt-12 outfit-600 text-xl">Minecraft game version does Wangyland server</p>
+          <p class="mt-12 outfit-600 text-xl ">Minecraft game version does Wangyland server</p>
           <p class="my-3 outfit-200">Wangyland supports Minecraft version: 1.19.3</p>
 
           <p class="mt-12 outfit-600 text-xl">Server IP for Wangyland</p>
@@ -25,7 +25,7 @@
           <img class="border-dashed border lg:hidden p-2 mt-12 border-gray-600" src="/img/1.png" alt="We Confident" />
         </div>
         <div class="">
-          <img class="hidden lg:block border-dashed border hover:border-double transition duration-1000 ease-in-out hover:border-indigo-400 hover:border-opacity-60 border-gray-600 p-4 rounded-2xl" src="/img/1.png" alt="We Confident" />
+          <img class="hidden lg:block border-dashed border hover:border-double transition duration-1000 ease-in-out backdrop-blur-sm bg-white/5 hover:bg-indigo-300/20 hover:border-indigo-400 hover:border-opacity-60 border-gray-600 p-4 rounded-2xl" src="/img/1.png" alt="We Confident" />
         </div>
       </div>
     </section>
