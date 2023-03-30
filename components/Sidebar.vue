85649a9080986c73b6281d47c55a54738940e303
devdewa <uchiasasuke123.sp@gmail.com>
2023-03-31 01:17:02 +0700

    Add listobx getting started in guide page on mobile version

--- components/Sidebar.vue
+++ components/Sidebar.vue
@@ -14,23 +14,42 @@
     <NuxtLink to="/guide/other" class="hover:bg-slate-700 text-sm outfit-200 py-3 pl-3 ml-2 bg-slate-800 border-gray-700 border-l guides">Other</NuxtLink>
   </div>
 
-  <div class="lg:hidden">
-    <Listbox v-model="selectedPerson">
-      <ListboxButton class="outfit-600 text-md py-3"><Icon class="mr-2 leading-none -mt-1" size="16" name="octicon:cpu-16" />Plugins</ListboxButton>
-        <NuxtLink :to="person.to" v-for="person in people">
-        <ListboxOptions>
-          <ListboxOption class="hover:bg-slate-700 text-sm outfit-200 py-3 pl-3 ml-2 bg-slate-800 border-gray-700 border-l guides"
-            :key="person.id"
-            :value="person"
-            :disabled="person.unavailable"
+  <div class="lg:hidden py-2">
+    <Listbox v-model="selectedGetting" v-slot="{ open }">
+      <ListboxButton class="outfit-600 text-md py-3"><Icon class="mr-2 leading-none -mt-1" size="16" name="octicon:play-16"/>Getting Started</ListboxButton>
+      <div v-show="open">
+        <ListboxOptions static>
+          <ListboxOption
+            v-for="getting in started"
+            :key="getting.id"
+            :value="getting"
+            :disabled="getting.unavailable"
           >
-            {{ person.name }}
+          <NuxtLink :to="getting.to" class="hover:bg-slate-700 text-sm outfit-200 py-3 pl-3 ml-2 bg-slate-800 border-gray-700 flex border-l guides"> {{ getting.name }} </NuxtLink>
           </ListboxOption>
         </ListboxOptions>
-      </NuxtLink>
+      </div>
     </Listbox>
   </div>
 
+  <div class="lg:hidden py-2">
+    <Listbox v-model="selectedPlugin" v-slot="{ open }">
+      <ListboxButton class="outfit-600 text-md py-3"><Icon class="mr-2 leading-none -mt-1" size="16" name="octicon:cpu-16" />Plugins</ListboxButton>
+      <div v-show="open">
+        <ListboxOptions static>
+          <ListboxOption
+            v-for="plugin in plugins" 
+            :key="plugin.id"
+            :value="plugin"
+            :disabled="plugin.unavailable"
+          >
+          <NuxtLink :to="plugin.to" class="hover:bg-slate-700 text-sm outfit-200 py-3 pl-3 ml-2 bg-slate-800 border-gray-700 flex border-l guides"> {{ plugin.name }} </NuxtLink>
+          </ListboxOption>
+        </ListboxOptions>
+      </div>
+    </Listbox>
+  </div>
+  
 </template>
 
 <script setup>
@@ -41,7 +60,12 @@
     ListboxOption,
   } from '@headlessui/vue'
 
-  const people = [
+  const started = [
+    { id: 1, name: 'Introduce', to: '/guide/introduce', unavailable: false},
+    { id: 2, name: 'Usage', to: '/guide/usage', unavailable: false},
+  ]
+
+  const plugins = [
     { id: 1, name: 'KIT', to: '/guide/kit', unavailable: false},
     { id: 2, name: 'Warp', to: '/guide/warp', unavailable: false},
     { id: 3, name: 'Land Claim', to: '/guide/claim', unavailable: false},
