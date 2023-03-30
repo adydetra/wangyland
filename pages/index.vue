26088894cecb6ba74f5153f1230e6e8427151a0a
devdewa <uchiasasuke123.sp@gmail.com>
2023-03-30 23:42:02 +0700

    Update home page responsive

--- pages/index.vue
+++ pages/index.vue
@@ -1,31 +1,31 @@
 <template>
     <!-- About Section -->
-    <section class="bg-slate-800 select-none">
-      <div class="container mx-auto flex justify-between items-center py-12 px-3 lg:px-0 ">
-        <div class="">
-          <h2 class="text-3xl lg:text-5xl outfit-600">About <text class="text-indigo-300">Wangyland</text></h2>
+    <section class="bg-slate-800 select-none grid grid-cols-6 container mx-auto py-12 px-3 lg:px-0">
+        <div class="col-span-6 lg:col-span-3 2xl:col-span-2 flex items-center">
+          <div class="">
+            <h2 class="text-3xl lg:text-5xl outfit-600">About <text class="text-indigo-300">Wangyland</text></h2>
 
-          <p class="hidden lg:block my-3 leading-7 outfit-200">
-            The Wangyland server is currently hosted in Indonesia. This You can play<br> Adventure, Arena, Discord, Economy, Land Claim, Ranks, SMP, Survival<br>
-            on the Wangyland server.
-          </p>
+            <p class="hidden lg:block my-3 leading-7 outfit-200">
+              The Wangyland server is currently hosted in Indonesia. This You can play<br> Adventure, Arena, Discord, Economy, Land Claim, Ranks, SMP, Survival<br>
+              on the Wangyland server.
+            </p>
 
-          <p class="lg:hidden text-sm my-3 leading-7 outfit-200 ">
-            The Wangyland server is currently hosted in Indonesia. This You can play Adventure, Arena, Discord, Economy, Land Claim, Ranks, SMP, Survival<br>
-            on the Wangyland server.
-          </p>
+            <p class="lg:hidden text-sm my-3 leading-7 outfit-200 ">
+              The Wangyland server is currently hosted in Indonesia. This You can play Adventure, Arena, Discord, Economy, Land Claim, Ranks, SMP, Survival<br>
+              on the Wangyland server.
+            </p>
 
-          <p class="mt-12 outfit-600 text-xl ">Minecraft game version does Wangyland server</p>
-          <p class="my-3 outfit-200">Wangyland supports Minecraft version: 1.19.3</p>
+            <p class="mt-12 outfit-600 text-xl ">Minecraft game version does Wangyland server</p>
+            <p class="my-3 outfit-200">Wangyland supports Minecraft version: 1.19.3</p>
 
-          <p class="mt-12 outfit-600 text-xl">Server IP for Wangyland</p>
-          <p class="my-3 outfit-200">wangylandid.aternos.me | port: 56345</p>
+            <p class="mt-12 outfit-600 text-xl">Server IP for Wangyland</p>
+            <p class="my-3 outfit-200">wangylandid.aternos.me | port: 56345</p>
 
-          <img class="border-dashed border lg:hidden p-2 mt-12 border-gray-600" src="/img/1.png" alt="We Confident" />
+            <img class="border-dashed border lg:hidden p-2 mt-12 border-gray-600" src="/img/1.png" alt="We Confident" />
+          </div>
         </div>
-        <div class="">
+        <div class="lg:col-span-3 2xl:col-span-4 flex justify-end">
           <img class="hidden lg:block border-dashed border hover:border-double transition duration-500 ease-in-out backdrop-blur-sm bg-white/5 hover:bg-indigo-300/20 hover:border-indigo-400 hover:border-opacity-60 border-indigo-400 p-4 rounded-2xl" src="/img/1.png" alt="We Confident" />
         </div>
-      </div>
     </section>
 </template>
