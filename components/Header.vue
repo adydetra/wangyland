4f52ff9413212b64961f8999082bbe90d8d6481a
devdewa <uchiasasuke123.sp@gmail.com>
2023-03-31 00:35:48 +0700

    Update header in responsive tablet version

--- components/Header.vue
+++ components/Header.vue
@@ -2,8 +2,8 @@
   <!-- Header -->
   <header class="bg-indigo-600 text-gray-900 py-3 px-4">
     <div class="container mx-auto flex justify-between items-center">
-      <p class="text-indigo-100 text-xs lg:text-lg outfit-800 me-4 mb-0"><Icon class="mr-3 -mt-1" name="🚀" />Welcome to Wangyland Website</p>
-      <p class="text-indigo-100 outfit-600 tracking-wide hidden lg:block animate-pulse">IP: WANGYLANDID.ATERNOS.ME<Icon class="ml-2 text-lg leading-none -mt-1" size="26" name="uim:pagelines" /></p>
+      <p class="text-indigo-100 text-xs md:text-lg outfit-800 me-4 mb-0"><Icon class="mr-3 -mt-1" name="🚀" />Welcome to Wangyland Website</p>
+      <p class="text-indigo-100 outfit-600 tracking-wide hidden md:block animate-pulse">IP: WANGYLANDID.ATERNOS.ME<Icon class="ml-2 text-lg leading-none -mt-1" size="26" name="uim:pagelines" /></p>
     </div>
   </header>
 </template>
