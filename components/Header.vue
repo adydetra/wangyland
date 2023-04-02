3193411786aa8caf8e9a6c7e6afea1b26359afbe
devdewa <uchiasasuke123.sp@gmail.com>
2023-04-03 02:55:07 +0700

    Better responsive header component

--- components/Header.vue
+++ components/Header.vue
@@ -4,7 +4,7 @@
     <div class="container mx-auto flex justify-between items-center">
       <p class="text-indigo-100 text-xs md:text-lg outfit-800 me-4 mb-0 hidden md:block"><Icon class="mr-3 -mt-1" name="🚀" />Welcome to Wangyland Website</p>
       <p class="text-indigo-100 text-xs md:text-lg outfit-800 me-4 mb-0 md:hidden"><Icon class="mr-3 -mt-1" name="🚀" />Welcome</p>
-      <p class="text-indigo-100 text-xs md:text-base outfit-800 tracking-wide animate-pulse md:block"><hidden class="hidden md:inline-block">IP:</hidden> WANGYLANDID.ATERNOS.ME<Icon class="ml-1 md:ml-2   -mt-1 w-4 h-4 lg:w-6 lg:h-6" name="uim:pagelines" /></p>
+      <p class="text-indigo-100 text-xs md:text-base outfit-800 tracking-wide animate-pulse md:block"><hidden class="hidden md:inline-block">IP:</hidden> WANGYLANDID.ATERNOS.ME<Icon class="ml-1 md:ml-2 -mr-1 -mt-1 w-4 h-4 lg:w-6 lg:h-6" name="uim:pagelines" /></p>
     </div>
   </header>
 </template>
