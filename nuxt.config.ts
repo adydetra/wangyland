8bf88693ec506896747dc1675581a0e1fc6ea919
devdewa <uchiasasuke123.sp@gmail.com>
2023-10-02 19:15:08 +0700

    feat: add nuxt image & nuxt content modules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,76 +1,94 @@
 export default defineNuxtConfig({
   app: {
     head: {
-      title: 'Wangyland',
+      title: "Wangyland",
       htmlAttrs: {
         lang: "en",
       },
       bodyAttrs: {
-        class: 'bg-slate-800'
+        class: "bg-slate-800",
       },
       meta: [
-        { charset: 'utf-8' },
-        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-        { hid: 'description', name: 'description', content: 'Welcome to Wangyland Website' }
-      ]
+        { charset: "utf-8" },
+        { name: "viewport", content: "width=device-width, initial-scale=1" },
+        { hid: "description", name: "description", content: "Welcome to Wangyland Website" },
+      ],
     },
   },
-  
-  modules: [
-    '@nuxtjs/tailwindcss',
-    '@nuxtjs/google-fonts',
-    'nuxt-icon',
-    'nuxt-headlessui',
-    '@vite-pwa/nuxt'
-  ],
+
+  modules: ["@nuxtjs/tailwindcss", "@nuxtjs/google-fonts", "@nuxt/image", "@nuxt/content", "nuxt-icon", "nuxt-headlessui", "@vite-pwa/nuxt"],
+
+  devtools: { enabled: false },
+
+  tailwindcss: {
+    viewer: false,
+  },
+
+  content: {
+    documentDriven: true,
+    highlight: {
+      theme: {
+        default: "monokai",
+        dark: "github-dark",
+      },
+    },
+  },
+
+  image: {
+    quality: 70,
+    format: ["webp"],
+  },
 
   headlessui: {
-    prefix: 'Headless'
+    prefix: "Headless",
   },
-  
+
   googleFonts: {
     families: {
       Outfit: [200, 300, 400, 500, 600, 800],
-    }
+    },
+    display: "swap",
+    prefetch: true,
+    preconnect: true,
   },
 
   pwa: {
-    registerType: 'autoUpdate',
+    registerType: "autoUpdate",
     manifest: {
       name: "Wangyland",
       short_name: "Wangyland",
-      theme_color: '#818cf8',
-      background_color: '#ffffff',
+      theme_color: "#818cf8",
+      background_color: "#ffffff",
       icons: [
         {
-          src: 'pwa/icon_64x64.png',
-          sizes: '64x64',
-          type: 'image/png',
+          src: "pwa/icon_64x64.png",
+          sizes: "64x64",
+          type: "image/png",
         },
         {
-          src: 'pwa/icon_144x144.png',
-          sizes: '144x144',
-          type: 'image/png',
+          src: "pwa/icon_144x144.png",
+          sizes: "144x144",
+          type: "image/png",
         },
         {
-          src: 'pwa/icon_192x192.png',
-          sizes: '192x192',
-          type: 'image/png',
+          src: "pwa/icon_192x192.png",
+          sizes: "192x192",
+          type: "image/png",
         },
         {
-          src: 'pwa/icon_512x512.png',
-          sizes: '512x512',
-          type: 'image/png',
+          src: "pwa/icon_512x512.png",
+          sizes: "512x512",
+          type: "image/png",
         },
       ],
     },
     workbox: {
-      navigateFallback: '/',
-      globPatterns: ['**/*.{js,css,html,png,svg,ico,webp}'],
+      navigateFallback: "/",
+      globPatterns: ["**/*.{js,css,html,png,svg,ico,webp}"],
     },
     devOptions: {
       enabled: true,
-      type: 'module',
+      type: "module",
     },
-  }
-})
+  },
+});
