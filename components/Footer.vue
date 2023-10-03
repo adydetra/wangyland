ea6d659cb3d66f604d507065efb60d6e702710cb
devdewa <uchiasasuke123.sp@gmail.com>
2023-10-03 09:56:46 +0700

    chore(Footer): update url

--- components/Footer.vue
+++ components/Footer.vue
@@ -2,7 +2,7 @@
   <section class="px-4 lg:px-0 border-y border-slate-900 mt-auto text-gray-400 font-light tracking-wide">
     <div class="container mx-auto py-12 flex justify-between">
       <div>
-        <NuxtLink to="https://adityawarman.vercel.app/" target="_blank" class="text-sm hover:text-indigo-300"
+        <NuxtLink to="https://adydetra.my.id" target="_blank" class="text-sm hover:text-indigo-300"
           ><Icon class="mr-2 text-lg leading-none -mt-1" size="16" name="octicon:octoface" />Powered By Dewa</NuxtLink
         >
       </div>
