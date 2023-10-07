76bfe02ddc2850286f80e30d9868fb813fd9bf85
devdewa <uchiasasuke123.sp@gmail.com>
2023-10-07 09:46:33 +0700

    feat(navigation): active class navigation

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -2,17 +2,39 @@
   <div class="container mx-auto px-4 lg:px-0">
     <nav class="bg-transparent py-7 flex justify-between">
       <NuxtLink class="flex gap-4 items-center" :to="{ name: 'index' }">
-        <NuxtImg src="/img/logo.png" alt="logo" class="w-10" placeholder/>
+        <NuxtImg src="/img/logo.png" alt="logo" class="w-10" placeholder />
         <p class="font-bold text-2xl outfit-600">Wangyland</p>
       </NuxtLink>
       <ul class="flex gap-16 items-center outfit-400">
-        <li class="text-gray-50 hover:text-indigo-300 hidden md:block">
-          <NuxtLink :to="{ name: 'index' }"><Icon class="mr-2 text-lg leading-none -mt-1" size="26" name="uim:cube" />Home</NuxtLink>
-        </li>
-        <li class="text-gray-50 hover:text-indigo-300">
-          <NuxtLink to="/get-started/introduction"><Icon class="mr-2 text-lg leading-none -mt-1" size="26" name="uim:horizontal-align-left" />Guide</NuxtLink>
+        <li
+          v-for="link in links"
+          :key="link.to"
+          :class="{
+            'text-gray-50 hover:text-indigo-300': route.path !== link.to,
+            'hidden md:block': link.to === '/',
+          }"
+        >
+          <NuxtLink
+            :to="link.to"
+            :class="{
+              'text-indigo-300': route.path === link.to,
+            }"
+          >
+            <Icon v-if="link.text === 'Guide'" :class="icon" size="26" name="uim:horizontal-align-left" />
+            <Icon v-else-if="link.text === 'Home'" :class="icon" size="26" name="uim:cube" />
+            {{ link.text }}
+          </NuxtLink>
         </li>
       </ul>
     </nav>
   </div>
 </template>
+
+<script setup>
+const route = useRoute();
+const icon = "mr-2 text-lg leading-none -mt-1";
+const links = [
+  { to: "/", text: "Home" },
+  { to: "/get-started/introduction", text: "Guide" },
+];
+</script>
