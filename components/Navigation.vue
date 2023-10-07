bb69fbec3431ed80ed9537e167235038b3123ecb
devdewa <uchiasasuke123.sp@gmail.com>
2023-10-08 05:56:04 +0700

    fix(navigation): logic code to active class guide

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -6,23 +6,28 @@
         <p class="font-bold text-2xl outfit-600">Wangyland</p>
       </NuxtLink>
       <ul class="flex gap-16 items-center outfit-400">
-        <li
-          v-for="link in links"
-          :key="link.to"
-          :class="{
-            'text-gray-50 hover:text-indigo-300': route.path !== link.to,
-            'hidden md:block': link.to === '/',
-          }"
-        >
+        <li :class="{ 'hidden md:block': route.path !== '' }">
           <NuxtLink
-            :to="link.to"
+            to="/"
             :class="{
-              'text-indigo-300': route.path === link.to,
+              'text-indigo-300': route.path === '/',
+              'text-gray-50 hover:text-indigo-300': route.path !== '/',
             }"
           >
-            <Icon v-if="link.text === 'Guide'" :class="icon" size="26" name="uim:horizontal-align-left" />
-            <Icon v-else-if="link.text === 'Home'" :class="icon" size="26" name="uim:cube" />
-            {{ link.text }}
+            <Icon :class="icon" size="26" name="uim:cube" />
+            Home
+          </NuxtLink>
+        </li>
+        <li>
+          <NuxtLink
+            to="/get-started/introduction"
+            :class="{
+              'text-indigo-300': route.path !== '/',
+              'text-gray-50 hover:text-indigo-300': route.path === '/',
+            }"
+          >
+            <Icon :class="icon" size="26" name="uim:horizontal-align-left" />
+            Guide
           </NuxtLink>
         </li>
       </ul>
@@ -30,11 +35,7 @@
   </div>
 </template>
 
-<script setup>
+<script setup lang="ts">
 const route = useRoute();
 const icon = "mr-2 text-lg leading-none -mt-1";
-const links = [
-  { to: "/", text: "Home" },
-  { to: "/get-started/introduction", text: "Guide" },
-];
 </script>
