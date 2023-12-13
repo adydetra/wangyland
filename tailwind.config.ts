0ba36681b0afd1c8175190c64515e1b6e350d524
devdewa <uchiasasuke123.sp@gmail.com>
2023-12-13 13:06:17 +0700

    refactor: remove unused code

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,8 +2,6 @@ import type { Config } from "tailwindcss";
 import defaultTheme from "tailwindcss/defaultTheme";
 
 export default <Partial<Config>>{
-  darkMode: "class",
-  content: ["./components/**/*.{vue,js}", "./content/**/*.{vue,js}", "./layouts/**/*.vue", "./pages/**/*.vue", "./nuxt.config.{js,ts}", "./app.vue"],
   theme: {
     extend: {
       fontFamily: {
