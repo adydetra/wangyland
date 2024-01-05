88e3947210e13cab116d94d27b9faba03908454c
devdewa <uchiasasuke123.sp@gmail.com>
2024-01-05 19:50:33 +0700

    chore: remove preload

--- pages/index.vue
+++ pages/index.vue
@@ -41,7 +41,6 @@ useHead({
       </div>
       <div class="lg:col-span-4 flex justify-end">
         <NuxtImg
-          preload
           class="object-cover p-4 rounded-2xl hidden lg:block border-dashed border hover:border-double transition duration-500 ease-in-out backdrop-blur-sm bg-white/5 hover:bg-indigo-300/20 hover:border-indigo-400 hover:border-opacity-60 border-indigo-400"
           src="/img/1.png"
           alt="We Confident"
